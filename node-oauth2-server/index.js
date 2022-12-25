af633b79cd068c7c6144e7d8e7cbe439bee8495c
raja0612 <rajashekhar.ramayampeta@gmail.com>
2022-12-25 15:29:24 -0500

    Added Oauth2.0 server set up code

---
+++ node-oauth2-server/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(cors({ credentials: true, origin: true }));
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const GITHUB_URL = process.env.GITHUB_URL;
+const PORT = 8080;
+
+/*
+The below method will be called after successful
+authentication,
+
+this redicrect url is configured in git hub oauth app
+http://localhost:8080/oauth/redirect
+
+*/
+
+app.get("/oauth/redirect", (req, res) => {
+    axios({
+      method: "POST",
+      url: `${GITHUB_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${req.query.code}`,
+      headers: {
+        Accept: "application/json",
+      },
+    }).then((response) => {
+        console.log(response);
+      res.redirect(
+        `http://localhost:3000?access_token=${response.data.access_token}`
+      );
+    });
+  });
+  
+
+  app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`);
+  });
